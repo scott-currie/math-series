5730ca99e8d389f520d857803cc775b676a20c0f
Scott Currie <vitriolik@gmail.com>
2018-11-27 17:27:34 -0800

    Added  lucas function and updated tests. All  passing.

--- series.py
+++ series.py
@@ -1,13 +1,30 @@
 def fibonacci(n):
     """Takes an int n as parameter and returns the nth Fibonacci number using an iterative method.
     """
+    if n == 0:
+        return 0
     fib = 1
-    prev = 1
-    for _ in range(2, n):
+    prev = 0
+    for _ in range(1, n):
         tmp = fib
         fib += prev
         prev = tmp
     return fib
 
+def lucas(n):
+    """Takes an int n as parameter and returns the nth Lucas number using an iterative method.
+    """
+    if n == 0:
+        return 2
+    luc = 1
+    prev = 2
+    for _ in range(1, n):
+        tmp = luc
+        luc += prev
+        prev = tmp
+    return luc
+
+
 if __name__ == '__main__':
-    fibonacci(5)
+    print(', '.join([str(fibonacci(i)) for i in range(10)]))
+    print(', '.join([str(lucas(i)) for i in range(10)]))
--- test_series.py
+++ test_series.py
@@ -1,4 +1,5 @@
 from series import fibonacci
+from series import lucas
 import pytest
 
 def test_fibonacci_exists():
@@ -28,5 +29,39 @@ def test_fibonacci_case_99():
     actual = 218922995834555169026
     assert fibonacci(a) == actual
 
+
+def test_fibonacci_case_0():
+    a = 0
+    actual = 0
+    assert fibonacci(a) == actual
+
+def test_lucas_case_5():
+    a = 5
+    actual = 11
+    assert lucas(a) == actual
+
+
+def test_lucas_case_1():
+    a = 1
+    actual = 1
+    assert lucas(a) == actual
+
+
+def test_lucas_case_17():
+    a = 17
+    actual = 3571
+    assert lucas(a) == actual
+
+
+def test_lucas_case_99():
+    a = 99
+    actual = 489526700523968661124
+    assert lucas(a) == actual
+
+
+def test_lucas_case_0():
+    a = 0
+    actual = 2
+    assert lucas(a) == actual
 
 # fib = 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
